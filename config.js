13504a06436a0c47b581f30add89ccfbade5243b
Jhfnetboy(Survivor) <jhfnetboy@gmail.com>
2022-09-10 11:12:34 +0800

    add user modal and apis

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 
 var config = {};
-
 config.mongodb = process.env.MONGODB_URI;
-
+console.log("ENV",process.env.MONGODB_URI)
+// config.mongodb = "mongodb+srv://soulwalletbackend:X1SYvvRqLIRu8mVX@cluster0.b66i3qq.mongodb.net/?retryWrites=true&w=majority";
+// config.mongodb = "mongodb://127.0.0.1:27017/soulwallet";
 module.exports = config;
