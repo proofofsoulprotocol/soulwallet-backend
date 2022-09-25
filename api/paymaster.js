b6fe7a84fa56e91a56213362d9587a3103402cc2
Jhfnetboy(Survivor) <jhfnetboy@gmail.com>
2022-09-25 16:57:11 +0800

    basic axios post added

--- api/paymaster.js
+++ api/paymaster.js
@@ -1,21 +1,24 @@
 var Account = require("../models/account");
-var commUtils = require("../utils/comm-utils");
 const config = require("../config");
 const axios = require("axios");
 
-// wallet_address, new_key, 
-async function triggerReplaceKey(req, rsp, next) {
+// wallet_address, new_key, recovery_records(guardian_address, signature)
+// It will invoked by the updateRecoveryRecord when the condition meets the 2/3.
+async function triggerReplaceKey(wallet_address, new_key, recovery_record) {
   
   const baseUrl = config.baseUrl;
-  
-  var exists = false;
-  const result = await Account.find({email: req.body.email});
-  if (result.length > 0) {
-    exists = true;
+  var postData = {
+    "wallet_address": wallet_address,
+    "new_key": new_key,
+    "recovery_record": recovery_record
   }
-  return commUtils.retRsp(rsp, 200, "", {
-    exists: exists
-  });
+  const methodName = "triggerReplaceOnChainKey";
+  const rtData = await axios.post(baseUrl+methodName, postData).then((response)=>{
+      console.log(response.data);
+    }).catch((error)=>{
+      console.log(error);
+    });
+  return rtData;
 }
 
 
